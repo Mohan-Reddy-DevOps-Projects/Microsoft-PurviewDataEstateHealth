7eb8a7f2e03a794cf26a46fdb59ec79774d46980
Blake Lassiter <bllassit@microsoft.com>
2023-10-21 01:16:18 +0000

    Merged PR 1185814: abstract http client factory
    ### Description:
    Use an abstract http client factory to recycle connections. Simplifies the logic of the client factory from metadata service and any other future clients.
    
    ### PR Checklist:
    - [ ] PR title prefix added. (ICM, S360, Hot-Fix, Cherry-pick etc.)
    - [ ] Dependent changes addressed. (Geneva config, Service Basics, Partner check-ins etc.)
    - [ ] Integration tests added/updated.
    
    ### Testing:
    - [x] Manual E2E Testing. (Local, DF, Dev, CI, Perf, X-Region, X-Tenant)
    - [ ] Automated E2E Testing. (Dev, UX)
    
    ## Type of change
    - [ ] Bug fix _(non-breaking change which fixes an issue)_
    - [x] New feature _(non-breaking change which adds functionality)_
    - [ ] Breaking change _(fix or feature that would cause existing functionality to be altered or to not work as expected)_
    - [ ] Infrastructure/Configuration
    
    Related work items: #2771490

---
+++ src/DataAccess/HttpClientBase.cs
@@ -0,0 +1,204 @@
+﻿// -----------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// -----------------------------------------------------------
+
+namespace Microsoft.Azure.Purview.DataEstateHealth.DataAccess;
+
+using Microsoft.Rest;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+
+using System.Text.Json;
+using Microsoft.Azure.Purview.DataEstateHealth.Loggers;
+
+/// <summary>
+/// Http Client handler
+/// </summary>
+public abstract class HttpClientBase<TBase> : ServiceClient<TBase>
+    where TBase : ServiceClient<TBase>
+{
+    /// <summary>
+    /// The api-version.
+    /// </summary>
+    public string ApiVersion { get; set; }
+
+    /// <summary>
+    /// The base URI.
+    /// </summary>
+    public Uri BaseUri { get; set; }
+
+    /// <summary>
+    /// The json serializer options.
+    /// </summary>
+    protected static readonly JsonSerializerOptions options = new()
+    {
+        Converters = {
+            new JsonStringEnumConverter(),
+        },
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// The service logger.
+    /// </summary>
+    protected readonly IDataEstateHealthLogger logger;
+
+    /// <summary>
+    /// The client name used to connect to the service.
+    /// </summary>
+    protected readonly string clientName;
+
+    /// <summary>
+    /// The base http client.
+    /// </summary>
+    /// <param name="logger">the logger.</param>
+    /// <param name="httpClient">the client itself.</param>
+    /// <param name="clientName">the name of the client.</param>
+    /// <param name="disposeHttpClient">should we dispose?</param>
+    public HttpClientBase(
+        IDataEstateHealthLogger logger,
+        HttpClient httpClient,
+        string clientName,
+        bool disposeHttpClient) : base(httpClient, disposeHttpClient)
+    {
+        this.logger = logger;
+        this.clientName = clientName;
+    }
+
+    /// <summary>
+    /// Get request
+    /// </summary>
+    /// <param name="endpoint"></param>
+    /// <param name="headers"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    protected async Task<HttpResponseMessage> GetAsync(
+        string endpoint,
+        IList<KeyValuePair<string, string>> headers,
+        CancellationToken cancellationToken)
+    {
+        return await this.SendAsync<object>(this.ConstructEndpoint(endpoint), HttpMethod.Get, headers, cancellationToken);
+    }
+
+    /// <summary>
+    /// Post request
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="endpoint"></param>
+    /// <param name="body"></param>
+    /// <param name="headers"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    protected async Task<HttpResponseMessage> PostAsync<TRequest>(
+        string endpoint,
+        TRequest body,
+        IList<KeyValuePair<string, string>> headers,
+        CancellationToken cancellationToken) where TRequest : class
+    {
+        return await this.SendAsync(this.ConstructEndpoint(endpoint), HttpMethod.Post, headers, cancellationToken, body);
+    }
+
+    /// <summary>
+    /// Delete request
+    /// </summary>
+    /// <param name="endpoint"></param>
+    /// <param name="headers"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    protected async Task<HttpResponseMessage> DeleteAsync(
+        string endpoint,
+        IList<KeyValuePair<string, string>> headers,
+        CancellationToken cancellationToken)
+    {
+        return await this.SendAsync<object>(this.ConstructEndpoint(endpoint), HttpMethod.Delete, headers, cancellationToken);
+    }
+
+    /// <summary>
+    /// Reads the response stream into the specified type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="stream"></param>
+    /// <param name="options"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    protected static async Task<T> DeserializeResponse<T>(Stream stream, JsonSerializerOptions options, CancellationToken cancellationToken)
+    {
+        MemoryStream ms = new();
+        await stream.CopyToAsync(ms, cancellationToken);
+        ms.Seek(0, SeekOrigin.Begin);
+
+        return JsonSerializer.Deserialize<T>(ms, options);
+    }
+
+    /// <summary>
+    /// Reads all characters from the current position to the end of the stream asynchronously and returns them as one string.
+    /// </summary>
+    /// <param name="stream"></param>
+    /// <returns></returns>
+    protected static async Task<string> ResponseToString(Stream stream)
+    {
+        using StreamReader reader = new(stream);
+
+        return await reader.ReadToEndAsync();
+    }
+
+    private async Task<HttpResponseMessage> SendAsync<TRequest>(
+        Uri endpoint,
+        HttpMethod method,
+        IList<KeyValuePair<string, string>> headers,
+        CancellationToken cancellationToken,
+        TRequest body = null) where TRequest : class
+    {
+        this.logger.LogInformation($"Client: {this.clientName} Method: {method} Endpoint: {endpoint.AbsoluteUri}");
+        using HttpRequestMessage request = await CreateRequest(endpoint, method, body, headers, cancellationToken);
+
+        return await this.HttpClient.SendAsync(request, cancellationToken);
+    }
+
+    private static HttpRequestMessage CreateRequest(Uri endpoint, HttpMethod method, IList<KeyValuePair<string, string>> headers)
+    {
+        var request = new HttpRequestMessage(method, endpoint);
+        if (headers?.Any() ?? false)
+        {
+            foreach (KeyValuePair<string, string> kvp in headers)
+            {
+                request.Headers.Add(kvp.Key, kvp.Value);
+            }
+        }
+
+        return request;
+    }
+
+    private static async Task<HttpRequestMessage> CreateRequest<T>(Uri endpoint, HttpMethod method, T body, IList<KeyValuePair<string, string>> headers, CancellationToken cancellationToken)
+    {
+        HttpRequestMessage request = CreateRequest(endpoint, method, headers);
+
+        if (body is HttpContent httpContent)
+        {
+            request.Content = httpContent;
+        }
+        else if (body != null)
+        {
+            MemoryStream ms = new();
+            await JsonSerializer.SerializeAsync(ms, body, options, cancellationToken);
+            ms.Seek(0, SeekOrigin.Begin);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var requestContent = new StreamContent(ms);
+            requestContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            request.Content = requestContent;
+        }
+
+        return request;
+    }
+
+    /// <summary>
+    /// Constructs a URI frm the base URI and the request path.
+    /// </summary>
+    /// <param name="path">The path of the endpoint. Must not include a leading forward slash.</param>
+    /// <returns></returns>
+    private Uri ConstructEndpoint(string path)
+    {
+        return new UriBuilder($"{this.BaseUri}{path}").Uri;
+    }
+}
--- src/DataAccess/MetadataStore/MetadataServiceClientFactory.cs
+++ src/DataAccess/MetadataStore/MetadataServiceClientFactory.cs
@@ -5,42 +5,21 @@
 namespace Microsoft.Azure.Purview.DataEstateHealth.DataAccess;
 
 using System;
-using System.Net;
+using System.Net.Http;
 using Microsoft.Azure.ProjectBabylon.Metadata;
 using Microsoft.Azure.Purview.DataEstateHealth.Configurations;
 using Microsoft.Azure.Purview.DataEstateHealth.Loggers;
 using Microsoft.Extensions.Options;
 
-/// <summary>
-/// Manages clients that communicate with the Purview metadata service.
-/// Clients are reused for a while, then recycled to allow the http client factory
-/// to control the lifetime of the http client handlers
-/// </summary>
-public class MetadataServiceClientFactory : IDisposable
+/// <inheritdoc/>
+internal sealed class MetadataServiceClientFactory : ClientFactory<ProjectBabylonMetadataClient>, IDisposable
 {
     private const string ApiVersion = "2019-11-01-preview";
     private readonly MetadataServiceConfiguration config;
-    private readonly IHttpClientFactory httpClientFactory;
-    private readonly IDataEstateHealthLogger logger;
-    private ProjectBabylonMetadataClient instance;
-    private ProjectBabylonMetadataClient previousInstance;
-    private long instanceCreationTime;
-    private bool disposedValue;
 
-    /// <summary>
-    /// Object reference to use for locking
-    /// </summary>
-    private readonly object lockObj = new object();
-
-    /// <summary>
-    /// The name of the metadata service http client to be referenced by the factory
-    /// </summary>
     public static string HttpClientName { get; } = "MetadataServiceClient";
 
-    /// <summary>
-    /// How often to recycle instance. Matches the client handler lifetime
-    /// </summary>
-    public static TimeSpan ClientLifetime { get; } = TimeSpan.FromHours(1);
+    protected override string ClientName => HttpClientName;
 
     /// <summary>
     /// Public constructor
@@ -48,75 +27,20 @@ public class MetadataServiceClientFactory : IDisposable
     /// <param name="config">Metadata client configuration</param>
     /// <param name="httpClientFactory">Http client factory</param>
     /// <param name="logger">Logger</param>
-    public MetadataServiceClientFactory(IOptions<MetadataServiceConfiguration> config, IHttpClientFactory httpClientFactory, IDataEstateHealthLogger logger)
+    public MetadataServiceClientFactory(
+        IOptions<MetadataServiceConfiguration> config,
+        IHttpClientFactory httpClientFactory,
+        IDataEstateHealthLogger logger) : base(httpClientFactory, logger)
     {
         this.config = config.Value;
-        this.httpClientFactory = httpClientFactory;
-        this.logger = logger;
     }
 
-    /// <summary>
-    /// Get the current metadata client
-    /// </summary>
-    /// <returns>Metadata service client</returns>
-    public ProjectBabylonMetadataClient GetClient()
+    protected override ProjectBabylonMetadataClient ConfigureClient(HttpClient httpClient)
     {
-        long lastCreationTime = Interlocked.Read(ref this.instanceCreationTime);
-        if (lastCreationTime + ClientLifetime.Ticks < DateTime.UtcNow.Ticks || this.instance == null)
+        return new ProjectBabylonMetadataClient(httpClient, true)
         {
-            lock (this.lockObj)
-            {
-                // Within the lock there's no need to use Interlocked to read the value
-                if (this.instanceCreationTime + ClientLifetime.Ticks < DateTime.UtcNow.Ticks || this.instance == null)
-                {
-                    this.previousInstance?.Dispose();
-                    this.previousInstance = this.instance;
-                    HttpClient httpClient = this.httpClientFactory.CreateClient(HttpClientName);
-                    httpClient.DefaultRequestVersion = HttpVersion.Version20;
-                    this.instance = new ProjectBabylonMetadataClient(httpClient, true)
-                    {
-                        BaseUri = new Uri($"https://{this.config.Endpoint}"),
-                        ApiVersion = ApiVersion,
-                    };
-
-                    Interlocked.Exchange(ref this.instanceCreationTime, DateTime.UtcNow.Ticks);
-                    this.logger.LogInformation("MetadataServiceClientFactory|New metadata service client created");
-                }
-            }
-        }
-
-        return this.instance;
+            BaseUri = new Uri($"https://{this.config.Endpoint}"),
+            ApiVersion = ApiVersion,
+        };
     }
-
-    #region IDisposable Support
-
-    /// <summary>
-    /// Dispose effective implementation
-    /// </summary>
-    /// <param name="disposing">True when invoked from Dispose, false from finalizer</param>
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!this.disposedValue)
-        {
-            if (disposing)
-            {
-                this.instance?.Dispose();
-                this.previousInstance?.Dispose();
-            }
-
-            this.disposedValue = true;
-        }
-    }
-
-    /// <summary>
-    /// Dispose of the factory
-    /// </summary>
-    public void Dispose()
-    {
-        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        this.Dispose(disposing: true);
-        GC.SuppressFinalize(this);
-    }
-
-    #endregion
 }
