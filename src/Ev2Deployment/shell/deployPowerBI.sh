29c06218fef9962f35739af3f962edd4e18834eb
Zach Madsen <zachmadsen@microsoft.com>
2023-11-15 23:34:09 +0000

    Merged PR 1208131: [Infrastructure] Add another rollout spec to deploy PBI files to a storage account
    ### Description:
    
    You can run the PBI rollout in our current release pipelines, e.g., [DF release pipeline](https://msdata.visualstudio.com/Purview%20Data%20Governance/_releaseProgress?releaseId=2937&_a=release-pipeline-progress). There's a different stage for running PBI rollouts vs the usual one.
    
    Ev2 doesn't allow rollout specs to omit resources defined in the service model. That's why there's a "hack" to turn every resource into a wait step in the PBI rollout spec. The alternative is splitting our Ev2 artifact root into multiple directories to use multiple service models. That seemed like more work right now. The approach here also matches what shared infra did for its multiple rollout specs.
    
    ### PR Checklist:
    - [x] PR title prefix added. (ICM, S360, Hot-Fix, Cherry-pick etc.)
    - [ ] Dependent changes addressed. (Geneva config, Service Basics, Partner check-ins etc.)
    - [ ] Integration tests added/updated.
    
    ### Testing:
    - [x] Manual E2E Testing. (Local, DF, Dev, CI, Perf, X-Region, X-Tenant)
    - [ ] Automated E2E Testing. (Dev, UX)
    
    ## Type of change
    - [ ] Bug fix _(non-breaking change which fixes an issue)_
    - [ ] New feature _(non-breaking change which adds functionality)_
    - [ ] Breaking change _(fix or feature that would cause existing functionality to be altered or to not work as expected)_
    - [x] Infrastructure/Configuration
    
    Related work items: #2817021

---
+++ src/Ev2Deployment/shell/deployPowerBI.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+if [ -z ${SUBSCRIPTION_ID+x} ]; then
+    echo "SUBSCRIPTION_ID is unset, unable to continue"
+    exit 1;
+fi
+
+
+if [ -z ${POWER_BI_ARCHIVE_SAS+x} ]; then
+    echo "POWER_BI_ARCHIVE_SAS is unset, unable to continue"
+    exit 1;
+fi
+
+if [ -z ${STORAGE_ACCOUNT_NAME+x} ]; then
+    echo "STORAGE_ACCOUNT_NAME is unset, unable to continue"
+    exit 1;
+fi
+
+if [ -z ${DESTINATION_CONTAINER_NAME+x} ]; then
+    echo "DESTINATION_CONTAINER_NAME is unset, unable to continue"
+    exit 1;
+fi
+
+# Exit when commands complete with non-0 exit codes and display the failing command and its code
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command completed with exit code $?."' EXIT
+
+# echo "Switch cloud"
+# az cloud set -n $Cloud
+
+echo "Login to Azure with the user-assigned MSI"
+echo az login --identity
+az login --identity
+
+echo "Set current subscription"
+echo az account set --subscription $SUBSCRIPTION_ID
+az account set --subscription $SUBSCRIPTION_ID
+
+echo "Install extension storage-preview"
+echo az extension add --name storage-preview
+az extension add --name storage-preview
+
+echo "Downloading powerbi.tar file from build output"
+mkdir powerbifiles
+echo "wget $POWER_BI_ARCHIVE_SAS -O powerbi.tar"
+wget $POWER_BI_ARCHIVE_SAS -O powerbi.tar
+
+echo "Untar powerbi.tar to directory powerbifiles"
+tar -C powerbifiles -xvf powerbi.tar
+
+echo "Creating container"
+echo az storage container create --auth-mode login --account-name $STORAGE_ACCOUNT_NAME --name $DESTINATION_CONTAINER_NAME
+az storage container create --auth-mode login --account-name $STORAGE_ACCOUNT_NAME --name $DESTINATION_CONTAINER_NAME
+
+echo "Uploading powerbi files"
+echo az storage blob upload-batch --auth-mode login --account-name $STORAGE_ACCOUNT_NAME --destination $DESTINATION_CONTAINER_NAME --source powerbifiles --overwrite
+az storage blob upload-batch --auth-mode login --account-name $STORAGE_ACCOUNT_NAME --destination $DESTINATION_CONTAINER_NAME --source powerbifiles --overwrite
+
+echo "PowerBI files uploaded to storage container successfully"
