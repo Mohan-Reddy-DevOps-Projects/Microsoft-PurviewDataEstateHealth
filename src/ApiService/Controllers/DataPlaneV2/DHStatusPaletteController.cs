deb555cb511fdf8ab6bd3e4aab5d0b2096d38c21
Yongjie Wang <yonw@microsoft.com>
2024-02-20 07:13:23 +0000

    Merged PR 1277589: Update api path
    ### Description:
    _<Describe proposed changes here...>_
    
    ### PR Checklist:
    - [ ] PR title prefix added. (ICM, S360, Hot-Fix, Cherry-pick etc.)
    - [ ] Dependent changes addressed. (Geneva config, Service Basics, Partner check-ins etc.)
    - [ ] Integration tests added/updated.
    
    ### Testing:
    - [ ] Manual E2E Testing. (Local, DF, Dev, CI, Perf, X-Region, X-Tenant)
    - [ ] Automated E2E Testing. (Dev, UX)
    
    ## Type of change
    - [ ] Bug fix _(non-breaking change which fixes an issue)_
    - [ ] New feature _(non-breaking change which adds functionality)_
    - [ ] Breaking change _(fix or feature that would cause existing functionality to be altered or to not work as expected)_
    - [ ] Infrastructure/Configuration
    
    ### Additional Details (optional)
    _<Add more details as necessary to help reviewers better understand the changes...>_
    
    Update api path

--- src/ApiService/Controllers/DataPlaneV2/DHStatusPaletteController.cs
+++ src/ApiService/Controllers/DataPlaneV2/DHStatusPaletteController.cs
@@ -17,7 +17,7 @@ using Newtonsoft.Json.Linq;
 
 [ApiController]
 [ApiVersion(ServiceVersion.LabelV2)]
-[Route("/statusPalette")]
+[Route("controls/statusPalette")]
 public class DHStatusPaletteController(DHStatusPaletteService statusPaletteService) : DataPlaneController
 {
     [HttpGet]
