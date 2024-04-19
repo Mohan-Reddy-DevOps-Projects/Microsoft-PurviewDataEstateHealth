13896ce614b4ce381d1b2814be4aa6bdfcf43fca
Jar An <jaran@microsoft.com>
2024-04-19 02:22:20 +0000

    Merged PR 1331169: Adjust score for empty data

--- src/DQServices/Adapters/RuleAdapter/Rules/DHSimpleRuleAdapter.cs
+++ src/DQServices/Adapters/RuleAdapter/Rules/DHSimpleRuleAdapter.cs
@@ -9,21 +9,6 @@ internal class DHSimpleRuleAdapter
         var fieldExp = RuleFieldAdapter.ToDqExpression(ruleAdapterContext, simpleRule.CheckPoint.Value);
         var valueExp = RuleValueAdapter.ToDqExpression(simpleRule.CheckPoint.Value, simpleRule.Operand);
 
-        var basicExp = SimpleRuleOperatorAdapter.ToDqExpression(simpleRule.Operator.Value, fieldExp, valueExp);
-
-        switch (simpleRule.CheckPoint)
-        {
-            case DHCheckPoint.DataProductRelatedAssetsHaveDQScore:
-                return $"isNull(DADQSDataAssetId) || ({basicExp})";
-            case DHCheckPoint.DataProductRelatedAssetsOwnerCount:
-                return $"isNull(ADODataAssetId) || ({basicExp})";
-            case DHCheckPoint.DataProductRelatedDataAssetsWithClassificationCount:
-                return $"isNull(DACDataAssetId) || ({basicExp})";
-            case DHCheckPoint.DataProductAllRelatedTermsMinimalDescriptionLength:
-            case DHCheckPoint.DataProductRelatedTermsDescriptionLength:
-                return $"isNull(DPTGlossaryTermId) || ({basicExp})";
-            default:
-                return basicExp;
-        };
+        return SimpleRuleOperatorAdapter.ToDqExpression(simpleRule.Operator.Value, fieldExp, valueExp);
     }
 }
