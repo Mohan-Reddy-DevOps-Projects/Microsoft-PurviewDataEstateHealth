da512b96d1c7957670de49ec48e8e2fcabb26b99
Yuanqiu Li <yuanqiuli@microsoft.com>
2024-06-13 03:14:01 +0000

    Merged PR 1376923: Fix the XLarge pool size null pointer issue
    ### Description:
    _<Describe proposed changes here...>_
    
    ### PR Checklist:
    - [ ] PR title prefix added. (ICM, S360, Hot-Fix, Cherry-pick etc.)
    - [ ] Dependent changes addressed. (Geneva config, Service Basics, Partner check-ins etc.)
    - [ ] Integration tests added/updated.
    
    ### Testing:
    - [ ] Manual E2E Testing. (Local, DF, Dev, CI, Perf, X-Region, X-Tenant)
    - [ ] Automated E2E Testing. (Dev, UX)
    
    ## Type of change
    - [ ] Bug fix _(non-breaking change which fixes an issue)_
    - [ ] New feature _(non-breaking change which adds functionality)_
    - [ ] Breaking change _(fix or feature that would cause existing functionality to be altered or to not work as expected)_
    - [ ] Infrastructure/Configuration
    
    ### Additional Details (optional)
    _<Add more details as necessary to help reviewers better understand the changes...>_
    
    Fix the XLarge pool size null pointer issue
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    This pull request is a bug fix, addressing a null pointer issue related to the XLarge pool size in the SparkPoolConfigs class.
    
    #### PR Summary
    The pull request resolves a null pointer exception by adding an XLarge pool size configuration to the SparkPoolConfigs class.
    - In the file `/src/Models/ResourceModels/Spark/SparkPoolConfigs.cs`, a new constant `XLarge` was added and a corresponding configuration was added to the `options` dictionary. This configuration specifies the properties for an XLarge Spark pool.

--- src/Models/ResourceModels/Spark/SparkPoolConfigs.cs
+++ src/Models/ResourceModels/Spark/SparkPoolConfigs.cs
@@ -9,14 +9,31 @@ namespace Microsoft.Azure.Purview.DataEstateHealth.Models.ResourceModels;
 /// </summary>
 public sealed class SparkPoolConfigs
 {
+    // @see: https://learn.microsoft.com/en-us/azure/synapse-analytics/spark/apache-spark-pool-configurations#node-sizes
+    /*
+    Size	                        vCore	Memory
+    Small	                        4	    32 GB
+    Medium	                        8	    64 GB
+    Large	                        16	    128 GB
+    XLarge	                        32	    256 GB
+    XXLarge	                        64	    432 GB
+    XXX Large (Isolated Compute)	80	    504 GB
+    */
     private const string Small = "small";
     private const string Medium = "medium";
     private const string Large = "large";
+    private const string XLarge = "xlarge";
+    private const string XXLarge = "xxlarge";
+    private const string XXXLarge = "xxxlarge";
+
     private static readonly Dictionary<string, SparkPoolConfigProperties> options = new(StringComparer.OrdinalIgnoreCase)
     {
         { Small, new SparkPoolConfigProperties(Small,"28g","28g",4,4) },
         { Medium, new SparkPoolConfigProperties(Medium,"56g","56g",8,8) },
-        { Large, new SparkPoolConfigProperties(Large,"112g","112g",16,16) }
+        { Large, new SparkPoolConfigProperties(Large,"112g","112g",16,16) },
+        { XLarge, new SparkPoolConfigProperties(XLarge,"224g","224g",32,32) },
+        { XXLarge, new SparkPoolConfigProperties(XXLarge,"378g","378g",64,64) },
+        { XXXLarge, new SparkPoolConfigProperties(XXXLarge,"441g","441g",80,80) }
     };
 
     /// <summary>
