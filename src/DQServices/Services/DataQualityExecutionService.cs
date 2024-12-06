c4e24fb77446866cff081a62f116c09ff780c47f
Dushyant Bhatt <dubhatt@microsoft.com>
2024-12-06 08:02:56 +0000

    Merged PR 1529205: Control jobs clean up.
    ### Description:
    _<Describe proposed changes here...>_
    
    ### PR Checklist:
    - [ ] PR title prefix added. (ICM, S360, Hot-Fix, Cherry-pick etc.)
    - [ ] Dependent changes addressed. (Geneva config, Service Basics, Partner check-ins etc.)
    - [ ] Integration tests added/updated.
    
    ### Testing:
    - [ ] Manual E2E Testing. (Local, DF, Dev, CI, Perf, X-Region, X-Tenant)
    - [ ] Automated E2E Testing. (Dev, UX)
    
    ## Type of change
    - [ ] Bug fix _(non-breaking change which fixes an issue)_
    - [ ] New feature _(non-breaking change which adds functionality)_
    - [ ] Breaking change _(fix or feature that would cause existing functionality to be altered or to not work as expected)_
    - [ ] Infrastructure/Configuration
    
    ### Additional Details (optional)
    _<Add more details as necessary to help reviewers better understand the changes...>_
    
    Control jobs clean up.
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Code cleanup
    
    #### PR Summary
    This pull request focuses on cleaning up the control jobs in the `DataQualityExecutionService`.
    - Removed temporary solution for processing the queue for specific accounts in `src/DQServices/Services/DataQualityExecutionService.cs`.
    
    Related work items: #3660904

--- src/DQServices/Services/DataQualityExecutionService.cs
+++ src/DQServices/Services/DataQualityExecutionService.cs
@@ -213,17 +213,6 @@ public class DataQualityExecutionService : IDataQualityExecutionService
         var fromDate = DateTimeOffset.UtcNow.AddHours(-12);
         var toDate = DateTimeOffset.UtcNow;
 
-        // TODO: Remove this after one week - temporary solution to process the queue for these accounts
-        List<string> accountIds = new List<string> 
-        {   "268675e1-07b2-4ee0-a752-0d54921cd84b",
-            "0c89ab95-b9d6-47d8-9101-913ded037ba6",
-            "3f97dbec-dcf4-4b96-b81f-29929c32d93c",
-            "f0029967-80f0-4dd5-9df1-cd17b8b24a67"
-        };
-        if (accountIds.Contains(accountId)) {
-            fromDate = DateTimeOffset.UtcNow.AddDays(-90);
-        }
-
         // KQL query string to check for relevant job logs
         var kqlDehDq =  $@"DEH_JobInitMapping_log_CL
                             | where AccountId_g == ""{accountId}"" and ControlId_g == ""{controlId}""
